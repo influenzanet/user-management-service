503f221e89d37d18d25c4190ebfbd9e7c83fd384
Clément Turbelin <clement.turbelin@gmail.com>
2022-09-30 14:48:24 +0200

    make db config func public (for tools)

--- internal/config/configs.go
+++ internal/config/configs.go
@@ -36,8 +36,8 @@ func InitConfig() Config {
 	conf.ServiceURLs.LoggingService = os.Getenv("ADDR_LOGGING_SERVICE")
 
 	conf.LogLevel = getLogLevel()
-	conf.UserDBConfig = getUserDBConfig()
-	conf.GlobalDBConfig = getGlobalDBConfig()
+	conf.UserDBConfig = GetUserDBConfig()
+	conf.GlobalDBConfig = GetGlobalDBConfig()
 	conf.Intervals = getIntervalsConfig()
 
 	rl, err := strconv.Atoi(os.Getenv("NEW_USER_RATE_LIMIT"))
@@ -58,11 +58,12 @@ func InitConfig() Config {
 	}
 	conf.ReminderToUnverifiedAccountsAfter = int64(reminderToUnverifiedAccountsAfter)
 
-	conf.WeekDayStrategy = getWeekDayStrategy()
+	conf.WeekDayStrategy = GetWeekDayStrategy()
 	return conf
 }
 
-func getWeekDayStrategy() utils.WeekDayStrategy {
+// Get Weekday attribution strategy
+func GetWeekDayStrategy() utils.WeekDayStrategy {
 
 	wday := os.Getenv(ENV_WEEKDAY_ASSIGNATION_WEIGHTS)
 	if wday == "" {
@@ -72,7 +73,10 @@ func getWeekDayStrategy() utils.WeekDayStrategy {
 	if err != nil {
 		log.Fatalf("%s : %s", ENV_WEEKDAY_ASSIGNATION_WEIGHTS, err)
 	}
-	return utils.CreateWeekdayWeightedStrategy(w)
+
+	strategy := utils.CreateWeekdayWeightedStrategy(w)
+	fmt.Println("Weekday Strategy: ", strategy.String())
+	return strategy
 }
 
 func getLogLevel() logger.LogLevel {
@@ -111,79 +115,4 @@ func getIntervalsConfig() models.Intervals {
 	}
 
 	return intervals
-}
-
-func getUserDBConfig() models.DBConfig {
-	connStr := os.Getenv("USER_DB_CONNECTION_STR")
-	username := os.Getenv("USER_DB_USERNAME")
-	password := os.Getenv("USER_DB_PASSWORD")
-	prefix := os.Getenv("USER_DB_CONNECTION_PREFIX") // Used in test mode
-	if connStr == "" || username == "" || password == "" {
-		log.Fatal("Couldn't read DB credentials.")
-	}
-	URI := fmt.Sprintf(`mongodb%s://%s:%s@%s`, prefix, username, password, connStr)
-
-	var err error
-	Timeout, err := strconv.Atoi(os.Getenv("DB_TIMEOUT"))
-	if err != nil {
-		log.Fatal("DB_TIMEOUT: " + err.Error())
-	}
-	IdleConnTimeout, err := strconv.Atoi(os.Getenv("DB_IDLE_CONN_TIMEOUT"))
-	if err != nil {
-		log.Fatal("DB_IDLE_CONN_TIMEOUT" + err.Error())
-	}
-	mps, err := strconv.Atoi(os.Getenv("DB_MAX_POOL_SIZE"))
-	MaxPoolSize := uint64(mps)
-	if err != nil {
-		log.Fatal("DB_MAX_POOL_SIZE: " + err.Error())
-	}
-
-	noCursorTimeout := os.Getenv(ENV_USE_NO_CURSOR_TIMEOUT) == "true"
-
-	DBNamePrefix := os.Getenv("DB_DB_NAME_PREFIX")
-
-	return models.DBConfig{
-		URI:             URI,
-		Timeout:         Timeout,
-		IdleConnTimeout: IdleConnTimeout,
-		NoCursorTimeout: noCursorTimeout,
-		MaxPoolSize:     MaxPoolSize,
-		DBNamePrefix:    DBNamePrefix,
-	}
-}
-
-func getGlobalDBConfig() models.DBConfig {
-	connStr := os.Getenv("GLOBAL_DB_CONNECTION_STR")
-	username := os.Getenv("GLOBAL_DB_USERNAME")
-	password := os.Getenv("GLOBAL_DB_PASSWORD")
-	prefix := os.Getenv("GLOBAL_DB_CONNECTION_PREFIX") // Used in test mode
-	if connStr == "" || username == "" || password == "" {
-		log.Fatal("Couldn't read DB credentials.")
-	}
-	URI := fmt.Sprintf(`mongodb%s://%s:%s@%s`, prefix, username, password, connStr)
-
-	var err error
-	Timeout, err := strconv.Atoi(os.Getenv("DB_TIMEOUT"))
-	if err != nil {
-		log.Fatal("DB_TIMEOUT: " + err.Error())
-	}
-	IdleConnTimeout, err := strconv.Atoi(os.Getenv("DB_IDLE_CONN_TIMEOUT"))
-	if err != nil {
-		log.Fatal("DB_IDLE_CONN_TIMEOUT" + err.Error())
-	}
-	mps, err := strconv.Atoi(os.Getenv("DB_MAX_POOL_SIZE"))
-	MaxPoolSize := uint64(mps)
-	if err != nil {
-		log.Fatal("DB_MAX_POOL_SIZE: " + err.Error())
-	}
-
-	DBNamePrefix := os.Getenv("DB_DB_NAME_PREFIX")
-
-	return models.DBConfig{
-		URI:             URI,
-		Timeout:         Timeout,
-		IdleConnTimeout: IdleConnTimeout,
-		MaxPoolSize:     MaxPoolSize,
-		DBNamePrefix:    DBNamePrefix,
-	}
 }
